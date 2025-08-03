41f67c5c38edc9b900815578776f603f39d9739f
Unchul <unchul87@gmail.com>
2025-08-03 16:05:59 +0900

    Host앱에서 layout설정 및 네비게이션 제작 [JSF-7]

---
+++ main-container/src/components/layout/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationBar from '../../nav/NavigationBar'
+
+const Header = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  )
+}
+
+export default Header
