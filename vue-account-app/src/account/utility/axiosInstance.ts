bb34bded3a6f395a082d31d4f4dd422c2bf7005a
GoldenDuck <unchul87@gmail.com>
2025-08-27 17:48:49 +0900

    feat:[임운철]마이페이지 연결과 간단한 스터디모임 디자인 수정 [JSF-42]

--- vue-account-app/src/account/utility/axiosInstance.ts
+++ vue-account-app/src/account/utility/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export function createAxiosInstances() {
     return {
         djangoAxiosInstance: axios.create({
-            baseURL: "http://localhost:8000", // 실서버 주소로 맞춰주세요
+            baseURL: "http://localhost:8080", // 실서버 주소로 맞춰주세요
             timeout: 7000,
             headers: {
                 "Content-Type": "application/json",
