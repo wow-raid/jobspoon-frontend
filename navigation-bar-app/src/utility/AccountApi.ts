d3bf46b04dff029c3650828fd55d14384115aec8
Hyunsoo Choi <gustn9025@naver.com>
2025-09-03 19:15:39 +0900

    chore: [최현수] log 및 배포 설정 세팅

--- navigation-bar-app/src/utility/AccountApi.ts
+++ navigation-bar-app/src/utility/AccountApi.ts
@@ -2,8 +2,12 @@ import springAxiosInst from "./AxiosInst.ts";
 
 
 export const logoutRequest = async () => {
-    console.log("로그인 진입")
+    const uri = springAxiosInst.getUri();
+    console.log("URL : " + uri);
+    console.log("로그인 진입");
     console.log("헤더 값 : " + `Bearer ${localStorage.getItem("userToken")}`);
+
+
     return await springAxiosInst.post('/api/account/logout',
             {},
             {
