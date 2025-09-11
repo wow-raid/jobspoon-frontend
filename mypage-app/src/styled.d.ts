cc583ba10f0e9571ad7a0b67e5d4ca61e9d4eb64
정은선 <jeong7653@naver.com>
2025-09-11 18:31:51 +0900

    chore: [정은선] 다크모드 적용

---
+++ mypage-app/src/styled.d.ts
@@ -0,0 +1,29 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        bg: string;
+        fg: string;
+        surface: string;
+        surfaceAlt: string;
+        surfaceHover: string;
+        border: string;
+        muted: string;
+        subtle: string;
+        primary: string;
+        primaryHover: string;
+        tagBg: string;
+        overlay: string;
+        inputBg: string;
+        inputBorder: string;
+        inputPlaceholder: string;
+        badgeRecruitingBg: string;
+        badgeRecruitingFg: string;
+        badgeClosedBg: string;
+        badgeClosedFg: string;
+        accent: string;
+        accentHover: string;
+        danger: string;
+        dangerHover: string;
+    }
+}
